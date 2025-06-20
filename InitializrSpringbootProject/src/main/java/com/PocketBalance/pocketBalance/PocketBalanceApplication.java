75e0971ca350c34f0a9371457c7aae439b062da9
JB1302 <jstevenbarrantes@gmail.com>
2025-06-20 17:28:07 -0600

    Deployment inicial y estructura de template

---
+++ InitializrSpringbootProject/src/main/java/com/PocketBalance/pocketBalance/PocketBalanceApplication.java
@@ -0,0 +1,13 @@
+package com.PocketBalance.pocketBalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketBalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketBalanceApplication.class, args);
+	}
+
+}
---
+++ InitializrSpringbootProject/src/main/java/com/PocketBalance/pocketBalance/ProjectConfig.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.PocketBalance.pocketBalance;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ * @author jstev
+ */
+public class ProjectConfig implements WebMvcConfigurer{
+    
+}
